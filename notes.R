64e8e6cd4b89498667daed133be828057b7410b9
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-12-08 11:58:30 -0500

    vectors, comparing and selecting

---
+++ notes.R
@@ -0,0 +1,11 @@
+# Vectors
+returns <- c(100, -60, -4, 70, 2)
+names(returns) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+"All"
+returns
+
+# Select
+winners <- returns[returns > 0]
+"Winners"
+winners
